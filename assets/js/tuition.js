55ba1765ce017f62a362da0762a086544d93bf2e
Ashish Kuamr <entropy.ash@gmail.com>
2018-06-27 16:07:05 +0530

    Admin view tuition page

--- assets/js/tuition.js
+++ assets/js/tuition.js
@@ -1,7 +1,7 @@
 let queryId = window.location.search.slice(1);
 
 $.ajax({
-    url: 'http://eduatlas.com/tuition',
+    url: '/tuition',
     data: {_id: queryId}
 }).then(updateThePage);
 
@@ -10,6 +10,31 @@ function updateThePage(data) {
     $('#tuition_name').html(data.name);
     $('#address').html(data.addressLine1 + ', ' + data.addressLine2);
     $('#phone').html(data.primaryNumber);
-    $('#email').html(data.email);
+    $('.email').html(data.email);
+    $('#description').html(data.description);
+    $('#contact_person').html(data.contactPerson);
+    $('#primary_number').html(data.primaryNumber);
+    $('#alternate_number').html(data.secondaryNumber);
+    $('#website').html(data.website);
 
+    doTheTemplateStuff(data);
+}
+
+function doTheTemplateStuff(data) {
+    const facilityArr = data.facilities.split(',');
+    const facilitySource = $('#facility_template').html();
+    const facilityTemplate = Handlebars.compile(facilitySource);
+    const facilityHtml = facilityTemplate({facilities: facilityArr});
+    $('#facilities_container').html(facilityHtml);
+
+    const categoryArr = data.facilities.split(',');
+    const categorySource = $('#category_template').html();
+    const categoryTemplate = Handlebars.compile(categorySource);
+    const categoryHtml = categoryTemplate({categories: categoryArr});
+    $('#category_container').html(categoryHtml);
+
+    const operationSource = $('#operates_on_template').html();
+    const operationTemplate = Handlebars.compile(operationSource);
+    const operationHtml = operationTemplate({dayAndTimeOfOperation: data.dayAndTimeOfOperation});
+    $('#opration_container').html(operationHtml);
 }
