ad81b878e5628a8a2f0735d6886140132323b58e
Ashish Kumar <entropy.ash@gmail.com>
2018-07-26 18:10:44 +0530

    Add: gallery

---
+++ assets/js/gallery.js
@@ -0,0 +1,8 @@
+lightbox.option({
+    alwaysShowNavOnTouchDevices: true,
+    disableScrolling: true,
+    fadeDuration: 300,
+    imageFadeDuration: 300,
+    resizeDuration: 200,
+    wrapAround: true
+});
