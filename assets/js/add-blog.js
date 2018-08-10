d71bf9ccf7aa55586ff7ff257eefb9547fa8c609
Ashish Kumar <entropy.ash@gmail.com>
2018-08-10 17:44:05 +0530

    Empty editor after blog is added

--- assets/js/add-blog.js
+++ assets/js/add-blog.js
@@ -4,7 +4,11 @@ function submitForm() {
         data: {
             body: tinymce.activeEditor.getContent()
         }
-    }).then(data => alert(`View your blogs at http://www.eduatlas.com/blog-viewer.html?id=${data._id}`)).catch(err => console.error(err));
+    }).then(data => {
+        alert(`View your blogs at http://www.eduatlas.com/blog-viewer.html?id=${data._id}`);
+        tinymce.activeEditor.setContent('');
+    })
+        .catch(err => console.error(err));
 }
 
 $('#save_btn').click(submitForm);
