6d7fc31dfe6d7b047f40317f02462bc5437d833d
Ashish Kumar <entropy.ash@gmail.com>
2018-08-20 14:25:14 +0530

    Minor formatting

--- assets/js/blog-show.js
+++ assets/js/blog-show.js
@@ -1,13 +1,13 @@
-let title=$('#title');
-let somedata=$('#data');
+let title = $('#title');
+let somedata = $('#data');
 $.get({
-    url:'/blog',
-    data: {
-        _id: '5b72c9b64c4f2e5676edb804'
-    }
+	url: '/blog',
+	data: {
+		_id: '5b72c9b64c4f2e5676edb804'
+	}
 }).then(render);
 
 function render(data) {
-   somedata.html(data.body)
-    title.html(data.title)
+	somedata.html(data.body)
+	title.html(data.title)
 }
