a8f9b2c3ebf1e987bdb24bd4dbc11ef1cced4049
RoBo_ <mannvender001@gmail.com>
2018-09-03 13:18:03 +0530

    Fix: no data card on dashboard

--- assets/js/dashboard-bookmarks.js
+++ assets/js/dashboard-bookmarks.js
@@ -39,15 +39,19 @@ const dashboardBookmarks = (() => {
             Promise.all(instituteInfoPromiseArr)
                 .then(instituteInfoArr => {
                     instituteInfoArr.forEach(instituteInfo => {
-                            const averageRating = helperScripts.calcAverageRating(instituteInfo.reviews);
-                            instituteInfo.averageRating = averageRating === -1 ? 2.5 : averageRating;
-                            instituteInfo.col4 = true;
-                            instituteInfo.hideFooter = true;
-                            instituteInfo.manageBookmarks = true;
-                            instituteInfo.typeOfInfo = typeOfInfo;
-                            cardsHtml += template.smoothCardHomePage(instituteInfo);
+                        const averageRating = helperScripts.calcAverageRating(instituteInfo.reviews);
+                        instituteInfo.averageRating = averageRating === -1 ? 2.5 : averageRating;
+                        instituteInfo.col4 = true;
+                        instituteInfo.hideFooter = true;
+                        instituteInfo.manageBookmarks = true;
+                        instituteInfo.typeOfInfo = typeOfInfo;
+                        if (instituteInfo.typeOfInfo === "event") {
+                            instituteInfo.event = true;
+                            instituteInfo.averageRating = null;
+                            instituteInfo.claimedBy = null;
                         }
-                    );
+                        cardsHtml += template.smoothCardHomePage(instituteInfo);
+                    });
                     resolve(cardsHtml);
                 }).catch(err => reject(err));
         });
@@ -67,8 +71,7 @@ const dashboardBookmarks = (() => {
             data: {
                 string: tuitionId
             }
-        }).then(() => {
-        })
+        }).then(() => {})
     }
 
     function eagerRemoveCard(cardId) {
@@ -79,23 +82,51 @@ const dashboardBookmarks = (() => {
     function render(user) {
         if (user.bookmarkTuitions) {
             getUserBookmarksHtml('tuition', user.bookmarkTuitions).then(cardsHtml => {
-                $userTuitionBookmarks.append(cardsHtml);
-                cacheDynamic();
-                bindEvents(user);
+                console.log(cardsHtml);
+                if (!cardsHtml) {
+                    const context = {
+                        title: "No Data",
+                        description: "bookmark tuitions you like and have everything arranged here."
+                    }
+                    cardsHtml = template.noDataCard(context);
+                    $userTuitionBookmarks.append(cardsHtml);
+                } else {
+                    $userTuitionBookmarks.append(cardsHtml);
+                    cacheDynamic();
+                    bindEvents(user);
+                }
             });
         }
         if (user.bookmarkSchools) {
             getUserBookmarksHtml('school', user.bookmarkSchools).then(cardsHtml => {
-                $userSchoolBookmarks.append(cardsHtml);
-                cacheDynamic();
-                bindEvents(user);
+                if (!cardsHtml) {
+                    const context = {
+                        title: "No Data",
+                        description: "bookmark schools you like and have everything arranged here."
+                    }
+                    cardsHtml = template.noDataCard(context);
+                    $userSchoolBookmarks.append(cardsHtml);
+                } else {
+                    $userSchoolBookmarks.append(cardsHtml);
+                    cacheDynamic();
+                    bindEvents(user);
+                }
             });
         }
         if (user.bookmarkEvents) {
             getUserBookmarksHtml('event', user.bookmarkEvents).then(cardsHtml => {
-                $userEventBookmarks.append(cardsHtml);
-                cacheDynamic();
-                bindEvents(user);
+                if (!cardsHtml) {
+                    const context = {
+                        title: "No Data",
+                        description: "bookmark evenrts you like and have everything arranged here."
+                    }
+                    cardsHtml = template.noDataCard(context);
+                    $userEventBookmarks.append(cardsHtml);
+                } else {
+                    $userEventBookmarks.append(cardsHtml);
+                    cacheDynamic();
+                    bindEvents(user);
+                }
             });
         }
     }
@@ -105,5 +136,7 @@ const dashboardBookmarks = (() => {
         render(user);
     }
 
-    return {init};
+    return {
+        init
+    };
 })();
