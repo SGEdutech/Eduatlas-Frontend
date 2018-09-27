33aba5bf144b3ab3fe3a2a30792b85f49e10e10b
Ashish Kumar <entropy.ash@gmail.com>
2018-09-27 13:47:46 +0530

    Remove required from add schools

--- assets/js/form.js
+++ assets/js/form.js
@@ -8,19 +8,19 @@ const instance = M.Modal.getInstance(elem);
 const form = $('#the_form');
 
 form.submit(e => {
-    e.preventDefault();
-    const formData = new FormData(form[0]);
-    $.ajax({
-        type: form.attr('method'),
-        url: form.attr('action'),
-        cache: false,
-        contentType: false,
-        processData: false,
-        data: formData,
-        success: data => {
-            instance.open();
-            $('input').not('.hide').val('');
-        },
-        error: err => console.error(err)
-    })
+	e.preventDefault();
+	const formData = new FormData(form[0]);
+	$.ajax({
+		type: form.attr('method'),
+		url: form.attr('action'),
+		cache: false,
+		contentType: false,
+		processData: false,
+		data: formData,
+		success: data => {
+			instance.open();
+			$('input').not('.hide').val('');
+		},
+		error: err => console.error(err)
+	})
 });
