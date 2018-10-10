a6a0962e877e8412f583c7ffa7604089d85c88be
RoBo_ <mannvender001@gmail.com>
2018-10-10 21:42:45 +0530

    Fix: bookmark button

--- assets/js/bookmark.js
+++ assets/js/bookmark.js
@@ -1,80 +1,80 @@
 const bookmark = (() => {
-    let userInfo;
-    let $loginModal;
-    let $bookmarkButtons;
+	let userInfo;
+	let $loginModal;
+	let $bookmarkButtons;
 
-    function bindEvents() {
-        $bookmarkButtons.click(saveBookmark)
-    }
+	function bindEvents() {
+		$bookmarkButtons.click(saveBookmark)
+	}
 
-    function cacheDynamicDom() {
-        $loginModal = $('#loginModal');
-        $bookmarkButtons = $('.bookmark-button')
-    }
+	function cacheDynamicDom() {
+		$loginModal = $('#loginModal');
+		$bookmarkButtons = $('.bookmark-button')
+	}
 
-    function updateUserInfo(info) {
-        userInfo = info;
-    }
+	function updateUserInfo(info) {
+		userInfo = info;
+	}
 
-    function saveBookmark() {
-        if (!userInfo) {
-            $loginModal.modal('show');
-        } else {
-            let instituteId = $(this).attr('data-id');
-            let typeOfInfo = $(this).attr('data-category');
-            let ifBookmarked = $(this).attr('data-bookmarked');
-            ifBookmarked = ifBookmarked == "true";
+	function saveBookmark() {
+		if (!userInfo) {
+			$loginModal.modal('show');
+		} else {
+			let instituteId = $(this).attr('data-id');
+			let typeOfInfo = $(this).attr('data-category');
+			let ifBookmarked = $(this).attr('data-bookmarked');
+			ifBookmarked = ifBookmarked == "true";
 
-            if (ifBookmarked) {
-                console.log("already bookmarked");
-                //do nothing
-                $(this).html('bookmark_border');
-                $(this).attr('data-bookmarked', 'false');
-                typeOfInfo = typeOfInfo.toLowerCase();
+			if (ifBookmarked) {
+				console.log("already bookmarked");
+				//do nothing
+				$(this).find('.material-icons').html('bookmark_border');
+				$(this).attr('data-bookmarked', 'false');
+				typeOfInfo = typeOfInfo.toLowerCase();
 
-                if (typeOfInfo === "tuition") {
-                    userApiCalls.deleteInArrayInUser(userInfo._id, "bookmarkTuitions", {
-                        string: instituteId
-                    }).catch(err => console.error(err))
-                } else if (typeOfInfo === "school") {
-                    userApiCalls.deleteInArrayInUser(userInfo._id, "bookmarkSchools", {
-                        string: instituteId
-                    }).catch(err => console.error(err))
-                } else {
-                    userApiCalls.deleteInArrayInUser(userInfo._id, "bookmarkEvents", {
-                        string: instituteId
-                    }).catch(err => console.error(err))
-                }
-            } else {
-                console.log("not bookmarked");
-                $(this).html('bookmark');
-                $(this).attr('data-bookmarked', 'true');
-                // make first character Upper-Case
-                typeOfInfo = typeOfInfo.toLowerCase();
-                if (typeOfInfo === "tuition") {
-                    userApiCalls.putInArrayInUser(userInfo._id, "bookmarkTuitions", {
-                        string: instituteId
-                    }).catch(err => console.error(err))
-                } else if (typeOfInfo === "school") {
-                    userApiCalls.putInArrayInUser(userInfo._id, "bookmarkSchools", {
-                        string: instituteId
-                    }).catch(err => console.error(err))
-                } else if(typeOfInfo === "event") {
-                    userApiCalls.putInArrayInUser(userInfo._id, "bookmarkEvents", {
-                        string: instituteId
-                    }).catch(err => console.error(err))
-                }
-            }
-        }
-    }
+				if (typeOfInfo === "tuition") {
+					userApiCalls.deleteInArrayInUser(userInfo._id, "bookmarkTuitions", {
+						string: instituteId
+					}).catch(err => console.error(err))
+				} else if (typeOfInfo === "school") {
+					userApiCalls.deleteInArrayInUser(userInfo._id, "bookmarkSchools", {
+						string: instituteId
+					}).catch(err => console.error(err))
+				} else {
+					userApiCalls.deleteInArrayInUser(userInfo._id, "bookmarkEvents", {
+						string: instituteId
+					}).catch(err => console.error(err))
+				}
+			} else {
+				console.log("not bookmarked");
+				$(this).find('.material-icons').html('bookmark');
+				$(this).attr('data-bookmarked', 'true');
+				// make first character Upper-Case
+				typeOfInfo = typeOfInfo.toLowerCase();
+				if (typeOfInfo === "tuition") {
+					userApiCalls.putInArrayInUser(userInfo._id, "bookmarkTuitions", {
+						string: instituteId
+					}).catch(err => console.error(err))
+				} else if (typeOfInfo === "school") {
+					userApiCalls.putInArrayInUser(userInfo._id, "bookmarkSchools", {
+						string: instituteId
+					}).catch(err => console.error(err))
+				} else if (typeOfInfo === "event") {
+					userApiCalls.putInArrayInUser(userInfo._id, "bookmarkEvents", {
+						string: instituteId
+					}).catch(err => console.error(err))
+				}
+			}
+		}
+	}
 
-    function init() {
-        cacheDynamicDom();
-        bindEvents();
-    }
+	function init() {
+		cacheDynamicDom();
+		bindEvents();
+	}
 
-    return {
-        init,
-        updateUserInfo
-    };
+	return {
+		init,
+		updateUserInfo
+	};
 })();
