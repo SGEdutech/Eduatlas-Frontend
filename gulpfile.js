5ea51e18ae45b0963c431109a93b994b33a18a7a
Ashish Kumar <entropy.ash@gmail.com>
2018-10-17 17:38:39 +0530

    Add: SuperAdmin update and delete

--- gulpfile.js
+++ gulpfile.js
@@ -12,36 +12,36 @@ const wrap = require('gulp-wrap');
 const declare = require('gulp-declare');
 
 gulp.task('minify', () =>
-    gulp.src('*.html')
-        .pipe(useref())
-        .pipe(gulpIf('*.js', babel({presets: ['env'], compact: false})))
-        .pipe(gulpIf('*.js', uglify()))
-        .on('error', err => gutil.log(gutil.colors.red('[Error]'), err.toString()))
-        .pipe(gulp.dest('dist'))
+	gulp.src('*.html')
+	.pipe(useref())
+	.pipe(gulpIf('*.js', babel({ presets: ['env'], compact: false })))
+	.pipe(gulpIf('*.js', uglify()))
+	.on('error', err => gutil.log(gutil.colors.red('[Error]'), err.toString()))
+	.pipe(gulp.dest('dist'))
 );
 
 gulp.task('imageMin', () =>
-    gulp.src('assets/img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'))
+	gulp.src('assets/img/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('dist/img'))
 );
 
-gulp.task('templates', function(){
-    gulp.src('templates/*.hbs')
-        .pipe(handlebars())
-        .pipe(wrap('Handlebars.template(<%= contents %>)'))
-        .pipe(declare({
-            namespace: 'template',
-            noRedeclare: true, // Avoid duplicate declarations
-        }))
-        .pipe(concat('templates.js'))
-        .pipe(gulp.dest('assets/js/'));
+gulp.task('templates', function() {
+	gulp.src('templates/*.hbs')
+		.pipe(handlebars())
+		.pipe(wrap('Handlebars.template(<%= contents %>)'))
+		.pipe(declare({
+			namespace: 'template',
+			noRedeclare: true, // Avoid duplicate declarations
+		}))
+		.pipe(concat('templates.js'))
+		.pipe(gulp.dest('assets/js/'));
 });
 
 gulp.task('watch', () => {
-    // gulp.watch('*.html', ['minify']);
-    // gulp.watch('assets/img/*', ['imageMin']);
-    gulp.watch('templates/*.hbs', ['templates']);
+	// gulp.watch('*.html', ['minify']);
+	// gulp.watch('assets/img/*', ['imageMin']);
+	gulp.watch('templates/*.hbs', ['templates']);
 });
 
 gulp.task('default', ['minify', 'imageMin', 'templates', 'watch']);
