06ba476e9c06ed91bd1b7000653ea051efaa49da
RoBo_ <mannvender001@gmail.com>
2018-11-19 19:06:04 +0530

    Fix: sort attendance by date and time

--- assets/js/study-monitor-attendance.js
+++ assets/js/study-monitor-attendance.js
@@ -22,30 +22,57 @@ const attendance = (() => {
 		return grouped;
 	}
 
-	function render() {
-		const splitObj = splitVariousArray(schedulesArr, 'batchId');
-		const keysArrOfSplitObj = Object.keys(splitObj);
+	function sortByDate(arr) {
+		arr.forEach(obj => console.log(obj.date))
+		arr.sort((a, b) => {
+			if (moment(a.date) > moment(b.date)) {
+				return -1;
+			} else if (moment(a.date) < moment(b.date)) {
+				return 1;
+			} else {
+				// date same , now campare with start time
+				if (a.fromTime > b.fromTime) {
+					return -1;
+				} else {
+					return 1;
+				}
+			}
+		})
+	}
+
+	function getHTML(splitObj, keysArrOfSplitObj) {
 		let attendaceCardsHTML = '';
 
 		keysArrOfSplitObj.forEach(key => {
+			sortByDate(splitObj[key]);
 			let numberOfPresents = 0;
+			let numberOfScheduled = 0;
 			splitObj[key].forEach(scheduleObj => {
 				const scheduleIsOld = moment(scheduleObj.date).isBefore(moment(Date.now()));
 				if (scheduleIsOld) {
 					if (scheduleObj.isAbsent === false) numberOfPresents++;
 				} else {
+					numberOfScheduled++;
 					scheduleObj.yetToCome = true;
 				}
 			});
-			const attendencePercentage = (numberOfPresents / splitObj[key].length) * 100;
+			let attendencePercentage = (numberOfPresents / (splitObj[key].length - numberOfScheduled)) * 100;
+			attendencePercentage = attendencePercentage.toFixed(2);
 			splitObj[key].forEach(scheduleObj => {
 				if (scheduleObj.date) {
 					scheduleObj.date = moment(scheduleObj.date).format('MMM Do');
 				}
 			})
-			console.log(splitObj[key]);
+			// console.log(splitObj[key]);
 			attendaceCardsHTML += template.attendanceCards({ schedules: splitObj[key], attendencePercentage });
 		});
+		return attendaceCardsHTML;
+	}
+
+	function render() {
+		const splitObj = splitVariousArray(schedulesArr, 'batchId');
+		const keysArrOfSplitObj = Object.keys(splitObj);
+		let attendaceCardsHTML = getHTML(splitObj, keysArrOfSplitObj);
 		$attendanceContainer.html(attendaceCardsHTML);
 	}
 
